7d2dd0c6e66efb85b9496856ed9d611560e01137
quantum.v.rl <quantum.v.rl@gmail.com>
2020-08-05 15:21:42 -0700

    socket svents

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,25 @@
-io()
+const socket = io()
+
+socket.on('message', (message) => {
+    console.log(message)
+})
+
+// document.querySelector('#message-form').addEventListener('submit', (e) => {
+//     e.preventDefault()
+
+//     const message = document.querySelector('input').value
+
+// socket.emit('sendMessage', message)
+// })
+
+
+document.querySelector('#message-form', function (e) {
+
+    mess.addEventListener('submit', (e) => {
+    e.preventDefault()
+    })
+
+    const message = document.querySelector('input').value
+
+    socket.emit('sendMessage', message)
+})
--- src/index.js
+++ src/index.js
@@ -12,10 +12,16 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('New Websocket connection')
+
+    socket.emit('message', 'Welcome Nomad!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
 })
 
-app.listen(port,() => {
+server.listen(port,() => {
     console.log(`Server is up on port ${port}!`)
 })
