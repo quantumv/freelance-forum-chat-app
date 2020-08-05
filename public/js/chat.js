b35e6fbf5af50a6bf5f95558934d7d73e0a4f2af
quantum.v.rl <quantum.v.rl@gmail.com>
2020-08-05 15:52:44 -0700

    second commit socket event

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,22 +4,22 @@ socket.on('message', (message) => {
     console.log(message)
 })
 
-// document.querySelector('#message-form').addEventListener('submit', (e) => {
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.element.message.value
+
+socket.emit('sendMessage', message)
+})
+
+
+// document.querySelector('#message-form', function (e) {
+
+//     mess.addEventListener('submit', (e) => {
 //     e.preventDefault()
+//     })
 
 //     const message = document.querySelector('input').value
 
-// socket.emit('sendMessage', message)
+//     socket.emit('sendMessage', message)
 // })
-
-
-document.querySelector('#message-form', function (e) {
-
-    mess.addEventListener('submit', (e) => {
-    e.preventDefault()
-    })
-
-    const message = document.querySelector('input').value
-
-    socket.emit('sendMessage', message)
-})
